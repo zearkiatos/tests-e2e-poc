a9ecf266d8d87db54694533ca52c3c687b871c80
migueltovar <loganitsolutions@outlook.com>
2023-05-06 21:34:59 -0500

    Escenario Creación de posts terminado

--- cypress/cypress/pages/SitePage.js
+++ cypress/cypress/pages/SitePage.js
@@ -1,10 +1,15 @@
 class SitePage {
     
     pagesLink=()=>cy.contains('Pages')
+    postsLink=()=>cy.contains('Posts')
 
 
     irAPaginas = () => {
         this.pagesLink().click()
+    }
+
+    irAPosts = () => {
+      this.postsLink().click()
     }
   
    
