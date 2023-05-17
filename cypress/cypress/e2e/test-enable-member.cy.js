94404bd3d6638361e867092cf5c1f28f34a24444
migue_vega <m.vegar@uniandes.edu.co>
2023-05-16 21:25:46 -0500

    Se carga implementación de Screenshots de activación de Members en Labs

--- cypress/cypress/e2e/test-enable-member.cy.js
+++ cypress/cypress/e2e/test-enable-member.cy.js
@@ -7,6 +7,9 @@ describe('Escenario para habilitar la opción de agregar miembros', () => {
   const signinPage = new SigninPage()
   const memberPage = new MemberPage()
 
+  const scenario="test-enabled-members";
+  const  testCase="enabled-members";
+
   beforeEach(function() {
   })
 
@@ -16,17 +19,37 @@ describe('Escenario para habilitar la opción de agregar miembros', () => {
 
     // Given
     cy.visit(this.user.urlLogin);
+    cy.wait(2000);
+    cy.screenshot(scenario+'/'+'1-'+testCase+'-Step-Labs-Members', {overwrite:true});
 
     // When
     signinPage.ingresarCorreoElectronico(this.user.usuario)
+    cy.wait(2000);
+    cy.screenshot(scenario+'/'+'2-'+testCase+'-Step-ingreso-correo', {overwrite:true});
+
     signinPage.ingresarPassword(this.user.contraseña)
+    cy.wait(1000);
+    cy.screenshot(scenario+'/'+'3-'+testCase+'-Step-ingreso-password', {overwrite:true});
+
     signinPage.hacerClicEnIniciarSesion()
+    cy.wait(1000);
+    cy.screenshot(scenario+'/'+'4-'+testCase+'-Step-click-inicio-sesion', {overwrite:true});
 
     sitePage.irALabs()
+    cy.wait(1000);
+    cy.screenshot(scenario+'/'+'5-'+testCase+'-Step-ir-a-Labs', {overwrite:true});
 
     memberPage.habilitarMiembros()
+    cy.wait(1000);
+    cy.screenshot(scenario+'/'+'5-'+testCase+'-Step-habilitar-Miembros', {overwrite:true});
+
     memberPage.activarOpcionMiembros()
+    cy.wait(1000);
+    cy.screenshot(scenario+'/'+'5-'+testCase+'-Step-Activar-Opcion', {overwrite:true});
+
     memberPage.guardarConfigurarion()     
+    cy.wait(1000);
+    cy.screenshot(scenario+'/'+'5-'+testCase+'-Step-Guardar-Configuracion', {overwrite:true});
     
     });     
   })
