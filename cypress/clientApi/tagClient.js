59888c17befff7b893aa6b7261fb35deb428b2c0
migueltovar <loganitsolutions@outlook.com>
2023-05-20 14:12:31 -0500

    escenario tag con descripción con más de 500 caracteres terminado

--- cypress/clientApi/tagClient.js
+++ cypress/clientApi/tagClient.js
@@ -1,4 +1,5 @@
 const MOCKAROO_API_URL = "https://my.api.mockaroo.com/tag_longer_name.json?key=0a3aea40";
+const MOCKAROO_API_URL_LONGER_DESCRIPTION = "https://my.api.mockaroo.com/tag_longer_description.json?key=0a3aea40";
 const headers = {
   "Content-Type": "application/json"
 };
@@ -12,5 +13,17 @@ export class TagClient {
       cy.wrap(response.body).as("getTags");
     });
     return cy.get("@getTags");
+  }
+
+
+  getTagsLongerDescription() {
+    cy.request({
+      method: "GET",
+      url: MOCKAROO_API_URL_LONGER_DESCRIPTION,
+      headers
+    }).then((response) => {
+      cy.wrap(response.body).as("getTagsLongerDescription");
+    });
+    return cy.get("@getTagsLongerDescription");
   }
 }
