d3b59741db880341bc9e790e88580ce78b776345
migueltovar <loganitsolutions@outlook.com>
2023-05-20 22:38:55 -0500

    Escenarios de invitación a miembros terminados

---
+++ cypress/cypress/e2e/random-tests/test-invite-member-with-invalid-email-random.cy.js
@@ -0,0 +1,72 @@
+import SitePage from '../../pages/SitePage'
+import SigninPage from '../../pages/SigninPage'
+import TagsPage from '../../pages/TagsPage'
+import TagsEditorPage from '../../pages/TagsEditorPage'
+import PagesPage from '../../pages/PagesPage'
+import PageEditorPage from '../../pages/PageEditorPage'
+import StaffPage from '../../pages/StaffPage'
+
+
+
+const { faker } = require('@faker-js/faker');
+
+describe('Escenario de invitación de miembros a Ghost', () => {
+  const sitePage = new SitePage()
+  const signinPage = new SigninPage()
+  const tagsPage = new TagsPage()
+  const tagsEditorPage = new TagsEditorPage()
+  const email= faker.random.alphaNumeric(5);
+  const pagesPage = new PagesPage()
+  const pageEditorPage = new PageEditorPage()
+  const staffPage = new StaffPage()
+
+  
+  beforeEach(function() {
+    
+    
+
+  })
+
+
+  
+
+  it('Invitar a un miembro a ghost con una dirección de correo no valida', () => {
+    cy.fixture('login-data.json').then(function (user) {
+      this.user = user;
+
+      // Given
+      cy.visit(this.user.urlLogin);
+
+
+      // When
+      signinPage.ingresarCorreoElectronico(this.user.usuario)
+      signinPage.ingresarPassword(this.user.contraseña)
+      signinPage.hacerClicEnIniciarSesion()
+      
+
+      sitePage.irAStaff()
+      
+
+      staffPage.nuevoMiembro()
+      
+      staffPage.ingresarEmail(email)
+      
+
+      staffPage.invitar()
+      
+      
+    // Then
+
+    cy.contains('Invalid Email').should('exist')
+      
+    
+    }); 
+
+    
+  })
+
+
+
+  
+
+})
